aa314453484bf6aab061b8bb551400a7d83b71a0
ylu021 <yichen_0@hotmail.com>
2017-10-12 12:00:56 -0400

    add spotify

---
+++ docs/creds.js
@@ -0,0 +1,2 @@
+const client_id = '8967c2c426c5405198eada0e5c226b82';
+const client_secret = '5a7142a88d1248989e97ea19903636e4';
--- docs/index.js
+++ docs/index.js
@@ -12,12 +12,119 @@
       detector.detectAllExpressions();
       detector.detectAllEmojis();
 
+      var token = '';
+
       //Add a callback to notify when the detector is initialized and ready for runing.
       detector.addEventListener("onInitializeSuccess", function() {
         log('#logs', "The detector reports initialized");
-        //Display canvas instead of video feed because we want to draw the feature points on it
+        // Display canvas instead of video feed because we want to draw the feature points on it
         $("#face_video").css("display", "none");
+        // Obtaining an spotify token
+        obtainToken();
       });
+
+      function obtainToken() {
+        console.log('obtaining token');
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://accounts.spotify.com/api/token', true);
+        xhr.setRequestHeader('Authorization', `Basic ${client_id}:${client_secret}`); // imported crendentials
+        xhr.setRequestHeader('Content-type', 'application/json');
+        xhr.send('grant_type=client_credentials');
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            localStorage.setItem('access_token', xhr.responseText['access_token']);
+          }else {
+            console.log('Spotify API Authorization Error');
+          }
+        }
+      }
+
+      function renewToken(done) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://accounts.spotify.com/api/token', true);
+        xhr.setRequestHeader('Authorization', `Basic ${client_id}:${client_secret}`); // imported crendentials
+        xhr.setRequestHeader('Content-type', 'application/json');
+        xhr.send('grant_type=client_credentials');
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            done(null, xhr.responseText['access_token']);
+          }else {
+            done('Token cannot be renewed', null);
+          }
+        }
+      }
+
+      function getDevice(done) {
+        let xhr = new XMLHttpRequest();
+        let token = localStorage.getItem('access_token');
+        xhr.open('GET', 'https://api.spotify.com/v1/me/player/devices', true);
+        xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+        xhr.setRequestHeader('Content-type', 'application/json');
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            done(null, xhr.responseText['devices'][0]['id']);
+          }else {
+            // TASK: retry
+          }
+        }
+      }
+
+      function searchPlaylist(keyword) {
+        console.log('searching playlist');
+        let token = localStorage.getItem('access_token');
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', `https://api.spotify.com/v1/search?q=${keyword}&type=playlist`, true);
+        xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+        xhr.setRequestHeader('Content-type', 'application/json');
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            // got my search
+            const items = xhr.responseText['playlists']['items'];
+            if(items && items.length > 0) {
+              // have result
+              let idx = Math.floor(Math.random() * (items.length-1));
+              let playlist = xhr.responseText['playlists']['items'][idx];
+              const playlistId = playlist['id'];
+              getDevice(function(err, deviceId) {
+                if(deviceId) {
+                  // devicePlay(playlistId, deviceId);
+                  let link = 'https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:' + playlistId;
+                  $('.container-fluid').append(`
+                    <iframe src="${link}" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
+                  `)
+                }
+              })
+            } else {
+              // no playlist result search for type=track
+            }
+          }else {
+            console.log('Spotify API Token Error');
+            renewToken(function (err, response) {
+              if(err) {
+                console.log(err);
+              }else {
+                localStorage.setItem('access_token', response);
+                searchPlaylist(keyword);
+              }
+            });
+          }
+        }
+      }
+
+      function devicePlay(playlistId, deviceId) {
+        let xhr = new XMLHttpRequest();
+        let token = localStorage.getItem('access_token');
+        xhr.open('PUT', 'https://api.spotify.com/v1/me/player/play', true);
+        xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+        xhr.send(`context_uri=spotify:playlist:${playlistId}`);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            done(null, xhr.responseText['devices'][0]['id']);
+          }else {
+            // TASK: retry
+          }
+        }
+      }
 
       function log(node_name, msg) {
         $(node_name).append("<span>" + msg + "</span><br />")
@@ -143,6 +250,7 @@
             }
             console.log(bgcolor);
             $('body').css('backgroundColor', bgcolor);
+            searchPlaylist(playlist);
             // if disgust play hiphop
             // if sad play ballad
             // if neutral play cheer up
